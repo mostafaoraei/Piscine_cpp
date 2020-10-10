9f02e71c9d817fbf3ca9c907a775358fab7d6f0d
Saeed Rajabalizade <saeedalize@gmail.com>
2020-10-10 16:42:41 +1100

    add contact file

---
+++ D0/PhonebookXcode/PhonebookXcode/Contact.cpp
@@ -0,0 +1,14 @@
+//
+//  Contact.cpp
+//  PhonebookXcode
+//
+//  Created by Saeed Rajabalizade on 10/10/20.
+//  Copyright © 2020 Saeed Rajabalizade. All rights reserved.
+//
+
+#include "Contact.hpp"
+
+class Contact{
+    
+    
+}
---
+++ D0/PhonebookXcode/PhonebookXcode/Contact.hpp
@@ -0,0 +1,14 @@
+//
+//  Contact.hpp
+//  PhonebookXcode
+//
+//  Created by Saeed Rajabalizade on 10/10/20.
+//  Copyright © 2020 Saeed Rajabalizade. All rights reserved.
+//
+
+#ifndef Contact_hpp
+#define Contact_hpp
+
+#include <stdio.h>
+
+#endif /* Contact_hpp */
