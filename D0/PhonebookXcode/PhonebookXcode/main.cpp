1045ef782d5746a5082dc818c3e9d8ee8f54ff7f
Saeed Rajabalizade <saeedalize@gmail.com>
2020-10-10 15:32:26 +1100

    add Xcode new project for phone book

---
+++ D0/PhonebookXcode/PhonebookXcode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PhonebookXcode
+//
+//  Created by Saeed Rajabalizade on 10/10/20.
+//  Copyright © 2020 Saeed Rajabalizade. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
