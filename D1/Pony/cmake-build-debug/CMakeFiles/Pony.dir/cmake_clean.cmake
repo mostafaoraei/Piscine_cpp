278d86aeb295b29ca1513646cc5a5b3b341b5644
mostafaoraei <taha.oraei@gmail.com>
2020-10-13 20:17:06 +0330

    init Pony

---
+++ D1/Pony/cmake-build-debug/CMakeFiles/Pony.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Pony.dir/main.cpp.o"
+  "Pony"
+  "Pony.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Pony.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
