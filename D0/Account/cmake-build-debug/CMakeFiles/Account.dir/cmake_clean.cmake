023eaf90bc9bc1536d740d789fd8ab04d8f51e71
mostafaoraei <taha.oraei@gmail.com>
2020-10-15 11:16:12 +0330

    add cmake file and main

---
+++ D0/Account/cmake-build-debug/CMakeFiles/Account.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "Account"
+  "Account.pdb"
+  "CMakeFiles/Account.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Account.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ D0/Account/main.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Mostafa Oraei on 10/15/20.
+//
+
+#include "iostream"
+
+int main() {
+    std::cout << "main" << std::endl;
+};
